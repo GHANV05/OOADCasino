4a3d6e2e3f6474c5794d45cd1588ef0dae51b7bc
Sarah Bian <sabi3820@colorado.edu>
2024-04-25 10:25:16 -0600

    mild fixes

--- src/main/java/casino/games/blackjack/Blackjack.java
+++ src/main/java/casino/games/blackjack/Blackjack.java
@@ -37,25 +37,6 @@ public class Blackjack extends Game {
         playersHand = new Hand();
         deck = new Deck(2, false);
 
-        int buyInChips = 0;
-        boolean validInput = false;
-        while (!validInput) {
-            System.out.println("How much money in chips would you like to buy? (minimum: $100)");
-            String input = scanner.nextLine();
-            try {
-                buyInChips = Integer.parseInt(input);
-                if (buyInChips >= 100) {
-                    validInput = true;
-                } else {
-                    System.out.println("Minimum buy-in is $100. Please enter a valid amount.");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a valid integer value.");
-            }
-        }
-
-        moneyGained -= buyInChips;
-
         System.out.println("+============================+");
         System.out.println("Beginning the game...");
         System.out.println("+============================+");
@@ -76,95 +57,187 @@ public class Blackjack extends Game {
                 System.out.println("Invalid input. Please enter a valid integer value.");
             }
         }
-
-        wager = firstWager;
+//
+//        wager = firstWager;
+//        moneyGained -= firstWager;
 
     }
+//
+//    private boolean gamePlayerHas21(){
+//
+//        if(turnCount == 0){
+//            System.out.println("Congrats! You got a Blackjack!");
+//            System.out.print("The dealer reveals his hand... ");
+//            for(Card card : dealersHand.returnCards()){
+//                System.out.print(card.getCardName());
+//            }
+//            if(dealersHand.calculateHandValue() == 21){
+//                System.out.println("The dealer also has a blackjack!");
+//                System.out.println("Your wager is returned to you. Good day!");
+//                moneyGained += wager;
+//            }
+//            else {
+//                System.out.println("You have won this game of Blackjack!");
+//                System.out.println("Your wager has been tripled and returned to you. Good day!");
+//                moneyGained += wager*3;
+//            }
+//        }
+//
+//        else {
+//            System.out.println("Congrats! You got a 21!");
+//            System.out.print("The dealer reveals his hand... ");
+//            for(Card card : dealersHand.returnCards()){
+//                System.out.print(card.getCardName());
+//            }
+//            if(dealersHand.calculateHandValue() < 17){
+//
+//                while(dealersHand.calculateHandValue() < 17){
+//                    dealersHand.addCard(deck.drawCard());
+//                    System.out.println("The dealer has drawn a card: " + dealersHand.returnCards().get(dealersHand.returnCards().size() - 1).getCardName());
+//                }
+//
+//            }
+//
+//            if(dealersHand.calculateHandValue() == 21){
+//                System.out.println("The dealer also has a 21.");
+//                System.out.println("Your wager is returned to you. Good day!");
+//                moneyGained = 0;
+//            }
+//
+//            else if(dealersHand.calculateHandValue() >= 21){
+//                System.out.println("The dealer has busted!");
+//                System.out.println("Your wager has been doubled and returned to you. Good day!");
+//                moneyGained += wager*2;
+//            }
+//
+//            else {
+//                System.out.println("Your hand has beaten the dealer!");
+//                System.out.println("Your wager has been doubled and returned to you. Good day!");
+//                moneyGained += wager*2;
+//            }
+//        }
+//
+//
+//        return true;
+//    }
 
 
     @Override
     protected boolean isGameOver() {
-        if(playersHand.calculateHandValue() == 21) {
-            return true;
-        }
-        else if(playersHand.calculateHandValue() > 21){
-            return true;
-        }
-        else if(playersHand.calculateHandValue() < 21 && dealersHand.calculateHandValue() < 17){
-            return false;
-        }
+
+//        if(playersHand.calculateHandValue() == 21) {
+//            return gamePlayerHas21();
+//        }
+//
+//        else if(playersHand.calculateHandValue() > 21){
+//            System.out.print("You're hand has busted!");
+//            System.out.println("You've lost your wager.");
+//            return true;
+//        }
+//
+//        else if(playersHand.calculateHandValue() < 21){
+//            System.out.print("The dealer reveals his hand... ");
+//            for(Card card : dealersHand.returnCards()){
+//                System.out.print(card.getCardName());
+//            }
+//            while(dealersHand.calculateHandValue() < 17){
+//                dealersHand.addCard(deck.drawCard());
+//                System.out.println("The dealer has drawn a card: " + dealersHand.returnCards().get(dealersHand.returnCards().size() - 1).getCardName());
+//            }
+//
+//            if(dealersHand.calculateHandValue() == playersHand.calculateHandValue() ){
+//                System.out.println("The dealer's hand is equivalent to yours.");
+//                System.out.println("Your wager is returned to you. Good day!");
+//                moneyGained = 0;
+//            }
+//
+//            else if(dealersHand.calculateHandValue() >= 21){
+//                System.out.println("The dealer has busted!");
+//                System.out.println("Your wager has been doubled and returned to you. Good day!");
+//                moneyGained += wager*2;
+//            }
+//
+//            else if(dealersHand.calculateHandValue() > playersHand.calculateHandValue()){
+//                System.out.println("The dealer's hand has beaten yours.");
+//                System.out.println("You've lost your wager.");
+//            }
+//
+//            else {
+//                System.out.println("Your hand has beaten the dealer!");
+//                System.out.println("Your wager has been doubled and returned to you. Good day!");
+//                moneyGained += wager*2;
+//            }
+//
+//            return false;
+//        }
         return false;
     }
 
     @Override
     protected void playTurn() {
+//
+//        if(turnCount == 0){
+//            System.out.println("Turn # " + turnCount);
 
-        if(turnCount == 0){
-            System.out.println("Turn # " + turnCount);
+//            dealersHand.addCard(deck.drawCard());
+//            System.out.println("The dealer has drawn a card...");
+//
+//            playersHand.addCard(deck.drawCard());
+//            System.out.println("You have drawn a card: " + playersHand.returnCards().get(0).getCardName());
+//
+//            dealersHand.addCard(deck.drawCard());
+//            System.out.println("The dealer has drawn a card: " + dealersHand.returnCards().get(1).getCardName());
+//
+//            playersHand.addCard(deck.drawCard());
+//            System.out.println("You have drawn a card: " +  playersHand.returnCards().get(1).getCardName());
+//
+//            boolean gameStatus = isGameOver();
 
-            dealersHand.addCard(deck.drawCard());
-            System.out.println("The dealer has drawn a card...");
-
-            playersHand.addCard(deck.drawCard());
-            System.out.println("You have drawn a card: " + playersHand.returnCards().get(0).getCardName());
-
-            dealersHand.addCard(deck.drawCard());
-            System.out.println("The dealer has drawn a card: " + dealersHand.returnCards().get(1).getCardName());
-
-            playersHand.addCard(deck.drawCard());
-            System.out.println("You have drawn a card: " +  playersHand.returnCards().get(1).getCardName());
-
-            boolean gameStatus = isGameOver();
-
-            turnCount++;
-        }
-
-        if(turnCount == 1){
-
-            System.out.println("Turn # " + turnCount);
-
-            System.out.println("-------------------------");
-            System.out.print("Your hand:  ");
-            for(Card card : playersHand.returnCards()){
-                System.out.print(card.getCardName());
-            }
-            System.out.println("-------------------------");
-            System.out.print("Dealer's hand: ");
-            for(Card card : dealersHand.returnCards()){
-                if(card == dealersHand.returnCards().get(0)){
-                    System.out.print("? of ?");
-                }
-                else {System.out.print(card.getCardName());}
-            }
-            System.out.println("-------------------------");
-            System.out.println("Select an option:");
-            System.out.println("-------------------------");
-            System.out.println("(A) Stand (do nothing)");
-            System.out.println("(B) Hit (take on another card)");
-            System.out.println("(C) Double Down (take on another card, double your wager)");
-            System.out.println("(D) Split (create two hands of cards of equal value");
-            System.out.println("-------------------------");
-            System.out.print("Type Your Option:");
-            String choice = scanner.nextLine().trim().toUpperCase();
-
-            switch (choice){
-                case "A":
-
-                    break;
-                case "B":
-
-                    break;
-                case "C":
-
-                    break;
-                case "D":
-
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
-            }
-
-        }
+//            turnCount++
+//            }
+//
+//        if(turnCount == 1){
+//
+//            System.out.println("Turn # " + turnCount);
+//
+//            System.out.println("-------------------------");
+//            System.out.print("Your hand:  ");
+//            for(Card card : playersHand.returnCards()){
+//                System.out.print(card.getCardName());
+//            }
+//            System.out.println("-------------------------");
+//            System.out.print("Dealer's hand: ");
+//            for(Card card : dealersHand.returnCards()){
+//                if(card == dealersHand.returnCards().get(0)){
+//                    System.out.print("? of ?");
+//                }
+//                else {System.out.print(card.getCardName());}
+//            }
+//            System.out.println("-------------------------");
+//            System.out.println("Select an option:");
+//            System.out.println("-------------------------");
+//            System.out.println("(A) Stand (do nothing)");
+//            System.out.println("(B) Hit (take on another card)");
+//            System.out.println("(C) Surrender (give up your hand");
+//            System.out.println("-------------------------");
+//            System.out.print("Type Your Option:");
+//            String choice = scanner.nextLine().trim().toUpperCase();
+//
+//            switch (choice){
+//                case "A":
+//
+//                    break;
+//                case "B":
+//
+//                    break;
+//                case "C":
+//
+//                    break;
+//                default:
+//                    System.out.println("Invalid option. Please try again.");
+//            }
+//
+//        }
 
     }
 
